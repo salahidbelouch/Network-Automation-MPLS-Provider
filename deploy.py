1cc8f9edcf7d06dcc5446e14d01a4f8deae63347
Salah <salah.idbelouch@insa-lyon.fr>
2023-01-27 11:58:11 +0100

    -deploy update

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 import json
 import gns3fy
+from gns3fy import Node
 from tabulate import tabulate
 import time
 
@@ -13,6 +14,8 @@ print(topo_data["topo"]["routers"][0]["ID"])
 # Connect to the GNS3 server
 server = gns3fy.Gns3Connector("http://10.56.67.183:3080")
 
+#verif -----
+
 print(
         tabulate(
             server.projects_summary(is_print=False),
@@ -20,23 +23,25 @@ print(
         )
     )
 
+
+# ouvre projet 
+
 projet= gns3fy.Project(name="Nouveau", connector=server)
 
+# get info 
 projet.get()
 print(projet)
 
+# variables projet
 id =projet.project_id
 c7200 =0
+
 projet.open()
 
 for template in server.get_templates():
      if "c7200" in template["name"]:
         print(f"Template: {template['name']} -- ID: {template['template_id']}")
         c7200=template['template_id']
-#projet.create()
-# creation projet : 
-
-print(projet.stats)
 
 
 
