e85e01e4384d744bd9afdf2edf53798bbc68a212
Abdeladem Fattah <dadem.fattah@gmail.com>
2023-03-17 00:46:01 +0100

    FINALEEE_

--- autoRouCo.py
+++ autoRouCo.py
@@ -174,7 +174,6 @@ if __name__ == '__main__':
     for node in projet.nodes:
         if node.name not in topo_data: # nouveau routeur qui n'a pas de conf sur json
             pass
-
         print("#####################")
         print(f"##### Node: {node.name} -- Node Type: {node.node_type} -- Status: {node.status} -- port {node.console} -- port {node.command_line}")
         tn = telnetlib.Telnet(ip, node.console)
@@ -206,11 +205,30 @@ if __name__ == '__main__':
             tn.write(b"\r")
             tn.write(b"conf t\r")
             tn.write(b"no ip icmp rate-limit unreachable\r")
-            tn.write(b"ip tcp synwait-time 5")
+            tn.write(b"ip tcp synwait-time 5\r")
             tn.write(b"ip cef\r")
             tn.write(b"end\r")
             time.sleep(0.3)
             print("ipcef DONE")
+        #VRF
+        if "VRF" in routeur:
+            for VRF in routeur["VRF"]:
+                NAME=VRF["name"]
+                RD=VRF["rd"]
+                IMPORT=VRF["rt_import"] #on a mis que c'est une liste au cas ou y en a plusieurs
+                EXPORT=VRF["rt_export"]
+                tn.write(b"\r")
+                tn.write(b"conf t\r")
+                tn.write(f"ip vrf {NAME}\r".encode())
+                tn.write(f"rd {RD}\r".encode())
+                for rt_in in IMPORT:
+                    tn.write(f"route-target import {rt_in}\r".encode())
+                for rt_out in EXPORT:
+                    tn.write(f"route-target export {rt_out}\r".encode())
+                # tn.write(b"address-family ipv4\r")
+                tn.write(b"end\r") 
+                time.sleep(0.7)
+                print(node.name,"VRF",NAME,"DONE")
         # Implementation interface par interface des parametres
         for int in routeur["interfaces"]:
             if int["Interface"]=="Loopback0":
@@ -258,26 +276,6 @@ if __name__ == '__main__':
                 else:
                     print(int["Interface"]," : ", node.name, "<->",int["With"],"MPLS DONE")
                 time.sleep(0.5)
-        #VRF
-        if "VRF" in routeur:
-            for VRF in routeur["VRF"]:
-
-                NAME=VRF["name"]
-                RD=VRF["rd"]
-                IMPORT=VRF["rt_import"] #on a mis que c'est une liste au cas ou y en a plusieurs
-                EXPORT=VRF["rt_export"]
-                tn.write(b"\r")
-                tn.write(b"conf t\r")
-                tn.write(f"ip vrf {NAME}\r".encode())
-                tn.write(f"rd {RD}\r".encode())
-                for rt_in in IMPORT:
-                    tn.write(f"route-target import {rt_in}\r".encode())
-                for rt_out in EXPORT:
-                    tn.write(f"route-target export {rt_out}\r".encode())
-                # tn.write(b"address-family ipv4\r")
-                tn.write(b"end\r") 
-                time.sleep(0.7)
-                print(node.name,"VRF",NAME,"DONE")
         #BGP
         if "BGP" in routeur:
             AS=routeur["BGP"]["AS"]
@@ -361,6 +359,8 @@ if __name__ == '__main__':
                 tn.write(b"conf t\r")
                 tn.write(b"int "+bytes(int["Interface"], "utf-8")+b"\r")
                 tn.write(f"ip vrf forwarding {int['VRF']}\r".encode())
+                tn.write(b"ip add "+bytes(int["Address"][0], "utf-8") +
+                    b" "+bytes(int["Address"][1], "utf-8")+b"\r")
                 tn.write(b"end\r")
         time.sleep(0.5)
         tn.write(b"end\r")
